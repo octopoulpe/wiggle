03fe5c3d5e10daea233cdcc4c4c97cd54e20247d
poulpe <hugouf@hotmail.fr>
2017-04-27 22:28:18 +0200

    new scenegraph ?

--- src/camera.js
+++ src/camera.js
@@ -2,51 +2,18 @@
 
 var mat4 = require("gl-matrix").mat4;
 var ctx = require("./context").ctx;
+var graph = require("./graph");
 
 
 var Camera = function (uniformName) {
-    this._viewMat = mat4.create();
     this._stagingViewMat = mat4.create();
     this._uniformName = uniformName;
-    this.x = 0;
-    this.y = 0;
-    this.z = 1;
-};
-
-Camera.prototype.set = function (x, y, z) {
-    if (x !== undefined) {
-        this.x = x;
-    }
-    if (y !== undefined) {
-        this.y = y;
-    }
-    if (z !== undefined) {
-        this.z = z;
-    }
-    mat4.identity(this._viewMat);
-    mat4.translate(
-        this._viewMat,
-        this._viewMat,
-        [this.x, this.y, this.z]
-    );
-};
-
-Camera.prototype.move = function (x, y, z) {
-    x = x || 0;
-    y = y || 0;
-    z = z || 0;
-    this.x += x;
-    this.y += y;
-    this.z += z;
-    mat4.translate(
-        this._viewMat,
-        this._viewMat,
-        [x, y, z]
-    );
+    this.node = new graph.GraphNode(new graph.Transform());
 };
 
 Camera.prototype.expose = function () {
-    mat4.invert(this._stagingViewMat, this._viewMat);
+    //console.log(this.stagingMat.getTranslation());
+    mat4.invert(this._stagingViewMat, this.node.getBubbledMat());
     ctx().shaderInUse.uniforms[this._uniformName](false, this._stagingViewMat);
 };
 
---
+++ src/graph.js
@@ -0,0 +1,64 @@
+"use strict";
+
+var mat4 = require("gl-matrix").mat4;
+var quat = require("gl-matrix").quat;
+
+
+var Transform = function () {
+    this.tX = 0;
+    this.tY = 0;
+    this.tZ = 0;
+    this.rX = 0;
+    this.rY = 0;
+    this.rZ = 0;
+    this.sX = 1;
+    this.sY = 1;
+    this.sZ = 1;
+    this.transformMat = mat4.create();
+    this.rotationQuat = quat.create();
+};
+
+Transform.prototype.refresh = function () {
+    var rotationQuat = this.rotationQuat;
+    quat.identity(rotationQuat);
+    quat.rotateX(rotationQuat, rotationQuat, this.rX);
+    quat.rotateY(rotationQuat, rotationQuat, this.rY);
+    quat.rotateZ(rotationQuat, rotationQuat, this.rZ);
+    mat4.fromRotationTranslationScale(
+        this.transformMat,
+        rotationQuat,
+        [this.tX, this.tY, this.tZ],
+        [this.sX, this.sY, this.sZ]
+    );
+};
+
+
+var GraphNode = function (transform) {
+    this.transform = transform;
+    this.parent = null;
+    this.stagingMat = mat4.create();
+};
+
+GraphNode.prototype.getBubbledMat = function () {
+    mat4.identity(this.stagingMat);
+    if (this.parent) {
+        mat4.multiply(
+            this.stagingMat,
+            this.parent.getBubbledMat(),
+            this.transform.transformMat
+        );
+    } else {
+        mat4.multiply(
+            this.stagingMat,
+            this.stagingMat,
+            this.transform.transformMat
+        );
+    }
+    return this.stagingMat;
+};
+
+
+module.exports = {
+    GraphNode: GraphNode,
+    Transform: Transform
+};
