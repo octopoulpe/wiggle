b65bb2e90e8b76f9f414d089e01849b3fd8c30b1
poulpe <hugouf@hotmail.fr>
2017-05-29 21:46:17 +0200

    added draft for primitives

---
+++ src/primitives.js
@@ -0,0 +1,32 @@
+
+var Sphere = function () {
+
+}
+
+var Cube = function () {
+
+}
+
+var Pill = function () {
+
+}
+
+var Pyramid = function () {
+
+}
+
+var Circle = function () {
+
+}
+
+var Square = function () {
+
+}
+
+var Oval = function () {
+
+}
+
+var Triangle = function () {
+
+}
